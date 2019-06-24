f7f203febe4d2796cbccf3fa4585b6ec6e9cd0de
Anthony Molina <antonio-molina@att.net>
2019-06-24 04:08:48 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -12,8 +12,8 @@ class Shoe
     puts "Your shoe is as good as new!"
   end
   
-  def brands=(brand, umique)
-    @brand.include? = brand
+  def brands=(brand)
+    @brand.include? = (brand)
     BRANDS << brand
 
 end
